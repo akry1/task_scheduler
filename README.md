6e4fcd37e2de1a06bb4b0d609527e508dcd0d5fa
akry1 <aravind_yempada@outlook.com>
2016-06-19 18:53:42 -0400

    added code for priority by created date

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![Build Status](https://travis-ci.org/PyBossa/random-scheduler.svg?branch=master)](https://travis-ci.org/PyBossa/random-scheduler)
 
-# random scheduler plugin for PyBossa
+# Custom scheduler plugin for PyBossa
+# Cloned from https://github.com/PyBossa/random-scheduler
--- custom_scheduler/__init__.py
+++ custom_scheduler/__init__.py
@@ -1,45 +1,75 @@
 import pybossa.sched as sched
 from pybossa.forms.forms import TaskSchedulerForm
-from pybossa.core import project_repo
 from flask.ext.plugins import Plugin
 from functools import wraps
-import random
 
 __plugin__ = "CustomScheduler"
 __version__ = "0.0.1"
 
-SCHEDULER_NAME = 'custom'
+SCHEDULER_NAME = 'Custom'
 
 
-def get_random_task(project_id, user_id=None, user_ip=None,
+#This method changes according to the priority
+def get_task_ids(project_id, user_id=None, user_ip=None):
+    """Get tasks based on the creation time for a given project and user."""
+    rows = None
+    if user_id and not user_ip:
+        query = sched.text('''
+                     SELECT id FROM task WHERE NOT EXISTS
+                     (SELECT task_id FROM task_run WHERE
+                     project_id=:project_id AND user_id=:user_id
+                        AND task_id=task.id)
+                     AND project_id=:project_id AND state !='completed'
+                     ORDER BY created DESC, id ASC LIMIT 10''')
+        rows = sched.session.execute(query, dict(project_id=project_id,
+                                           user_id=user_id))
+    else:
+        if not user_ip:
+            user_ip = '127.0.0.1'
+        query = sched.text('''
+                     SELECT id FROM task WHERE NOT EXISTS
+                     (SELECT task_id FROM task_run WHERE
+                     project_id=:project_id AND user_ip=:user_ip
+                        AND task_id=task.id)
+                     AND project_id=:project_id AND state !='completed'
+                     ORDER BY created DESC, id ASC LIMIT 10''')
+        rows = sched.session.execute(query, dict(project_id=project_id,
+                                           user_ip=user_ip))
+
+    return [t.id for t in rows]
+
+
+def get_task(project_id, user_id=None, user_ip=None,
                     n_answers=30, offset=0):
     """Return a random task for the user."""
-    project = project_repo.get(project_id)
-    if project and len(project.tasks) > 0:
-        return random.choice(project.tasks)
-    else:
+    #project = project_repo.get(project_id)
+    candidate_task_ids = get_task_ids(project_id, user_id, user_ip)
+    total_remaining = len(candidate_task_ids) - offset
+    if total_remaining <= 0:
         return None
+    return sched.session.query(sched.Task).get(candidate_task_ids[offset])
 
 
-def with_random_scheduler(f):
+
+def with_custom_scheduler(f):
     @wraps(f)
     def wrapper(project_id, sched, user_id=None, user_ip=None, offset=0):
         if sched == SCHEDULER_NAME:
-            return get_random_task(project_id, user_id, user_ip, offset=offset)
+            return get_task(project_id, user_id, user_ip, offset=offset)
         return f(project_id, sched, user_id=user_id, user_ip=user_ip, offset=offset)
     return wrapper
 
 
-def variants_with_random_scheduler(f):
+def variants_with_custom_scheduler(f):
     @wraps(f)
     def wrapper():
-        return f() + [(SCHEDULER_NAME, 'Random')]
+        return f() + [(SCHEDULER_NAME, 'Custom')]
     return wrapper
 
 
-class RandomScheduler(Plugin):
+class CustomScheduler(Plugin):
 
     def setup(self):
-        sched.new_task = with_random_scheduler(sched.new_task)
-        sched.sched_variants = variants_with_random_scheduler(sched.sched_variants)
+        sched.new_task = with_custom_scheduler(sched.new_task)
+        sched.sched_variants = variants_with_custom_scheduler(sched.sched_variants)
         TaskSchedulerForm.update_sched_options(sched.sched_variants())
